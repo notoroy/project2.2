3e7130c4ff49e384c3298b11cb9a8b1cf5af0621
notoroy <pasaroy2@gmail.com>
2022-12-29 16:57:36 +0300

    finished project 2

--- wiki/wiki/encyclopedia/urls.py
+++ wiki/wiki/encyclopedia/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>" , views.entrypage , name="entrypage"),
-   
-
+    path("search/" ,views.search , name="search"),
+    path("new/", views.new , name="new"), 
+    path("edit/",views.edit ,name="edit"),
+    path("save/" , views.save , name="save"),
+    path("random/" ,views.random_choice , name="randomchoice"),  
 ]
--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render , redirect
+from django.shortcuts import render 
 import markdown
 from . import util
+import random
 
 def conversion(title):
     content = util.get_entry(title)
@@ -24,7 +25,7 @@ def entrypage(request , title):
     content = conversion(title)
 
     if content == None:
-        return render(request , "encyclopedia/index.html",{
+        return render(request , "encyclopedia/error.html",{
             "message" : "The  entry input does not exist, here are some that do.",
             "entries" : util.list_entries()
         })
@@ -34,6 +35,94 @@ def entrypage(request , title):
             "content" : content , 
             "title" : title
         })
+
+def search(request):
+    if request.method == "POST":
+        search = request.POST["q"]
+
+        #Convert the input into html
+        content = conversion(search)
+
+        #Check whether the entry exists in the encyclopedia
+        if content is not None:
+            return render(request , "encyclopedia/entrypage.html",{
+                "content" : content , 
+                "title" : search
+        })
+
+        else:
+            #Get all the encyclopedia entries present in the storage.
+            entries = util.list_entries()
+            recommendation = []
+            #Go through each and every entry checking.
+            for entry in entries:
+                if search.lower() in entry.lower():
+                    recommendation.append(entry)
+
+            return render(request , "encyclopedia/search.html",{
+                "recommendation" : recommendation 
+            })
+
+
+        
+
+def new(request):
+    if request.method == "GET":
+        return render(request , "encyclopedia/new.html")
+
+    else:
+        title = request.POST["title"]
+        content = request.POST["content"]
+        exist = util.get_entry(title)
+        if exist is not None:
+            return render(request , "encyclopedia/error.html",{
+                "message" : "The entry you are trying to create already exists"
+            })
+        
+        else:
+            util.save_entry(title , content)
+            htmlcontent = conversion(title)
+            return render(request , "encyclopedia/entrypage.html",{
+                "title" : title , 
+                "content" : htmlcontent
+            })
+
+
+def edit(request):
+    if request.method == "POST":
+        title = request.POST["entry"]
+        content = util.get_entry(title)
+        return render(request , "encyclopedia/edit.html", {
+            "title" : title ,
+            "content" : content
+        })
+
+def save(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        util.save_entry(title , content)
+        htmlcontent = conversion(title)
+        return render(request , "encyclopedia/entrypage.html",{
+            "title" : title , 
+            "content" : htmlcontent
+        })
+
+
+def random_choice(request):
+    entries = util.list_entries()
+    randomchoice = random.choice(entries)
+    content = conversion(randomchoice)
+    return render(request , "encyclopedia/entrypage.html", {
+        "title" : randomchoice , 
+        "content" : content
+
+    })
+
+  
+
+
+
 
 
     
